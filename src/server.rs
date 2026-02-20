a3945a9cd09baf9564eb89a68de87b492108772c
Thanassis Polizogopoulos <polizogopoulos@iridalabs.com>
2026-02-20 16:44:17 +0200

    Simple echo server

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,40 @@
-// use crate::command::Command;
-// use crate::db::{Db, Value};
-// use std::sync::Arc;
-// use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-// use tokio::sync::Mutex;
 
 pub async fn run(addr: &str) {
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Rusteze is running on {}", addr);
 
     loop {
-        let (socket, _) = listener.accept().await.unwrap();
+        let (socket, addr) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            handle_connection(socket, addr).await;
         });
     }
 }
 
-async fn handle_connection(_socket: tokio::net::TcpStream) {
-    println!("New Connection")
-    // TODO: handle client
+async fn handle_connection(mut socket: tokio::net::TcpStream, addr: std::net::SocketAddr) {
+    println!("New connection from {}", addr);
+    let mut buf = vec![0u8; 512];
+
+    loop {
+        match socket.read(&mut buf).await {
+            Ok(0) => {
+                println!("Client {} disconnected", addr);
+                return;
+            }
+            Ok(n) => {
+                let msg = String::from_utf8_lossy(&buf[..n]);
+                print!("Received from {}: {}", addr, msg);
+
+                if socket.write_all(&buf[..n]).await.is_err() {
+                    return;
+                }
+            }
+            Err(e) => {
+                eprintln!("Error reading from {}: {}", addr, e);
+                return;
+            }
+        }
+    }
 }
