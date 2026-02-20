2a8b0f138fef9c534908940c85296c87cc5818db
Thanassis Polizogopoulos <polizogopoulos@iridalabs.com>
2026-02-20 15:52:29 +0200

    TCP server init

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,6 @@
-// use rusteze::command::Command;
-// use rusteze::db::{Db, Value};
-// use std::io::{self, Write};
-
 use rusteze::server;
 
 #[tokio::main]
 async fn main() {
-    // server::run("127.0.0.1:6379").await;
-    println!("Hello from server")
+    server::run("127.0.0.1:6969").await;
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,23 @@
-use crate::command::Command;
-use crate::db::{Db, Value};
-use std::sync::Arc;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+// use crate::command::Command;
+// use crate::db::{Db, Value};
+// use std::sync::Arc;
+// use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use tokio::sync::Mutex;
+// use tokio::sync::Mutex;
+
+pub async fn run(addr: &str) {
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("Rusteze is running on {}", addr);
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+        });
+    }
+}
+
+async fn handle_connection(_socket: tokio::net::TcpStream) {
+    println!("New Connection")
+    // TODO: handle client
+}
